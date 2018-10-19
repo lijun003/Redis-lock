9462435b427d764a9cfa35b8b8381c2f91877d3e
jun.li <jun.li@57blocks.io>
2018-10-19 14:44:50 +0800

    first commit

---
+++ src/main/java/com/redis/dlock/DlockApplication.java
@@ -0,0 +1,12 @@
+package com.redis.dlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlockApplication.class, args);
+	}
+}
